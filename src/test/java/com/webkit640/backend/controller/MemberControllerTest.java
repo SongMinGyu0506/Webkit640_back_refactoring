e24acefb0d3b9449a37dfd5c0032ad8b0ffe42b4
SongMinGyu <ggp04114@naver.com>
2023-04-29 04:15:28 +0900

    Test: 회원목록 열람기능 테스트코드 추가

--- src/test/java/com/webkit640/backend/controller/MemberControllerTest.java
+++ src/test/java/com/webkit640/backend/controller/MemberControllerTest.java
@@ -2,6 +2,7 @@ package com.webkit640.backend.controller;
 
 import com.webkit640.backend.dto.request.LoginDtoRequest;
 import com.webkit640.backend.dto.request.SignupDtoRequest;
+import com.webkit640.backend.dto.response.AllMemberDtoResponse;
 import com.webkit640.backend.dto.response.LoginDtoResponse;
 import com.webkit640.backend.entity.Member;
 import com.webkit640.backend.repository.MemberRepository;
@@ -12,8 +13,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
+
+import java.util.LinkedHashMap;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -64,6 +67,7 @@ class MemberControllerTest {
     }
     
     @Test
+    @DisplayName("로컬 로그인 테스트")
     void login() {
         String name = "test_name";
         String email = "test@test.com";
@@ -99,8 +103,45 @@ class MemberControllerTest {
     }
 
     @Test
+    @DisplayName("회원 목록 열람")
     void viewMembers() {
+        String name = "test_name";
+        String email = "test@test.com";
+        String password = "1234";
+        String memberBelong = "Student";
+        String memberType = "ADMIN";
 
+        SignupDtoRequest dto = SignupDtoRequest.builder()
+                .name("test_name")
+                .email("test@test.com")
+                .password("1234")
+                .memberBelong("Student")
+                .memberType("ADMIN")
+                .build();
+
+        String url = "http://localhost:" + port + "/auth/sign-up";
+        ResponseEntity<SignupDtoRequest> response = restTemplate.postForEntity(url,dto,SignupDtoRequest.class);
+
+        LoginDtoRequest dtos = LoginDtoRequest.builder()
+                .email("test@test.com")
+                .password("1234")
+                .build();
+        String urls = "http://localhost:"+port+"/auth/login";
+        ResponseEntity<LoginDtoResponse> responses = restTemplate.postForEntity(urls,dtos,LoginDtoResponse.class);
+        LoginDtoResponse body = responses.getBody();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization","Bearer "+body.getToken());
+        HttpEntity request = new HttpEntity(headers);
+        String requestUrl = "http://localhost:"+port+"/auth/view-members";
+        ResponseEntity<List> responseFinal = restTemplate.exchange(requestUrl,HttpMethod.GET,request,List.class);
+
+        LinkedHashMap<String,Object> lhm = (LinkedHashMap<String, Object>) responseFinal.getBody().get(0);
+        assertThat(lhm.get("email").toString()).isEqualTo("test@test.com");
+        assertThat(lhm.get("memberBelong").toString()).isEqualTo("Student");
+        assertThat(lhm.get("memberType").toString()).isEqualTo("ADMIN");
+        assertThat(lhm.get("name").toString()).isEqualTo("test_name");
+        assertThat(lhm.get("admin")).isEqualTo(false);
     }
 
 }
