440687bd5a30674d8f4a3a49fb0e691a8916b9a8
SongMinGyu <ggp04114@naver.com>
2023-04-29 16:46:56 +0900

    feat: Response 통합 DTO 생성

---
+++ src/main/java/com/webkit640/backend/dto/response/ResponseWrapper.java
@@ -0,0 +1,41 @@
+package com.webkit640.backend.dto.response;
+
+
+import lombok.*;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class ResponseWrapper<T> {
+    private LocalDate time;
+    private HttpStatus status;
+    private List<T> data;
+
+
+//    public ResponseWrapper addObject(T obj) {
+//        List<T> dataList = new ArrayList<>();
+//        dataList.add(obj);
+//        return ResponseWrapper.<T>builder()
+//                .status(HttpStatus.OK)
+//                .data(dataList)
+//                .time(LocalDate.now())
+//                .build();
+//    }
+
+    public static <T> ResponseWrapper addObject(T obj) {
+        List<T> dataList = new ArrayList<>();
+        dataList.add(obj);
+        return ResponseWrapper.<T>builder()
+                .status(HttpStatus.OK)
+                .data(dataList)
+                .time(LocalDate.now())
+                .build();
+    }
+}
