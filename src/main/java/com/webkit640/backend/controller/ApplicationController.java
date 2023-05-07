d90933a319ab5697c4cae44c5828664a397c64b9
SongMinGyu <ggp04114@naver.com>
2023-05-08 02:54:58 +0900

    Modify: ApplicationController API 네이밍 수정 및 기능추가
    상세내용:
    POST /application --> /application/submit
    GET /application --> /application/file/{email}
    신규추가
    GET /application/file --> zip 다운로드

--- src/main/java/com/webkit640/backend/controller/ApplicationController.java
+++ src/main/java/com/webkit640/backend/controller/ApplicationController.java
@@ -1,5 +1,7 @@
 package com.webkit640.backend.controller;
 
+import com.webkit640.backend.config.annotation.Admin;
+import com.webkit640.backend.config.exception.FileServiceException;
 import com.webkit640.backend.dto.ApplicationDto;
 import com.webkit640.backend.dto.response.ResponseWrapper;
 import com.webkit640.backend.entity.Applicant;
@@ -41,15 +43,15 @@ public class ApplicationController {
         this.fileEntityService = fileEntityService;
     }
 
-    @PostMapping(value = "",consumes = {"multipart/form-data"})
+    @PostMapping(value = "/submit",consumes = {"multipart/form-data"})
     public ResponseEntity<?> apply(@AuthenticationPrincipal int id, @RequestPart MultipartFile multipartFile, @RequestPart ApplicationDto dto) throws IOException {
         HashMap<String,Object> tmp = applicationService.saveEntity(id, ApplicationDto.dtoToEntity(dto));
         FileEntity fileEntity = fileEntityService.saveApplicationFile(multipartFile, (Applicant) tmp.get("applicant"), (Member) tmp.get("member"));
 
         return ResponseEntity.created(
-                ServletUriComponentsBuilder.fromCurrentRequest()
-                .path("/{id}")
-                .buildAndExpand(tmp.get("applicationId"))
+                ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path("/application/{id}")
+                .buildAndExpand(((Applicant)tmp.get("applicant")).getMember().getEmail())
                 .toUri())
                 .body(
                         ResponseWrapper.addObject(ApplicationDto.ApplicationResponseDto
@@ -58,18 +60,39 @@ public class ApplicationController {
                                 HttpStatus.CREATED)
                 );
     }
-    @GetMapping(value = "")
-    public ResponseEntity<?> downloadApplication(@AuthenticationPrincipal int id, @RequestParam String email) throws UnsupportedEncodingException {
+    @Admin
+    @GetMapping("/file/{email}")
+    public ResponseEntity<?> adminDownloadApplication(@AuthenticationPrincipal int id, @PathVariable String email) {
         Map<String, Object> map = fileEntityService.applicationDownload(id, email);
 
         File file = (File) map.get("file");
         Resource resource = (Resource) map.get("resource");
-        String fileName = new String(file.getName().getBytes(StandardCharsets.UTF_8),"ISO-8859-1");
+        String fileName = new String(file.getName().getBytes(StandardCharsets.UTF_8), StandardCharsets.ISO_8859_1);
 
         return ResponseEntity.ok()
                 .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + fileName +"\";")
                 .header(HttpHeaders.CONTENT_LENGTH, String.valueOf(file.length()))
                 .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_OCTET_STREAM.toString())
                 .body(resource);
+    }
+
+    @Admin
+    @GetMapping("/file")
+    public ResponseEntity<?> adminZipDownloadApplication(@AuthenticationPrincipal int id) {
+        Resource file = fileEntityService.filesToZip();
+        try {
+            return ResponseEntity.ok()
+                    .header(HttpHeaders.CONTENT_DISPOSITION,"attachment; filename=\""+file.getFile().getName() + "\"")
+                    .header(HttpHeaders.CONTENT_LENGTH,String.valueOf(file.getFile().length()))
+                    .header(HttpHeaders.CONTENT_TYPE,MediaType.APPLICATION_OCTET_STREAM.toString())
+                    .body(file);
+        } catch (IOException e) {
+            throw new FileServiceException("controller get file error");
+        }
+    }
+
+    @GetMapping("")
+    public ResponseEntity<?> viewApplicantList(@AuthenticationPrincipal int id, @RequestParam String year, @RequestParam String month) {
+        return null;
     }
 }
