c305890cbd9e376f12c0d7bc409c604188c51326
SongMinGyu <ggp04114@naver.com>
2023-05-16 23:14:15 +0900

    Fix: 누락사항 추가

--- src/main/java/com/webkit640/backend/repository/TraineeRepository.java
+++ src/main/java/com/webkit640/backend/repository/TraineeRepository.java
@@ -6,9 +6,11 @@ import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface TraineeRepository extends JpaRepository<Trainee,Integer>, JpaSpecificationExecutor<Trainee> {
     Trainee findById(int id);
     List<Trainee> findAll(Specification<Trainee> keyword);
